8cdacc96d10783f18596fd6069b12e81e6f4f70e
Deepak Kumar <dkarya.delhi.uk@gmail.com>
2022-01-18 16:21:35 +0530

    Added tree traversal example

--- 12. DSA(Basics)/Tree/Tree Traversal/Program.java
+++ 12. DSA(Basics)/Tree/Tree Traversal/Program.java
@@ -79,30 +79,126 @@ public class Program {
 
     // Level Order Traversal Brute Force Method
     public static void levelOrderTraversal(Node root) {
-        //height is used to know how many levels are there in the Tree.
+        // height is used to know how many levels are there in the Tree.
         int height = heightOfTree(root);
 
-
-        //We can print each level by using printKthLevel function.
+        // We can print each level by using printKthLevel function.
         for (int i = 0; i < height; i++) {
             printKthLevel(root, i);
         }
     }
 
-    //Level Order Traversal
-    public static void printLevel(Node root){
+    // Level Order Traversal
+    public static void printLevel(Node root) {
         Queue<Node> q = new LinkedList<Node>();
         q.add(root);
-        while(q.isEmpty() == false){
+        while (q.isEmpty() == false) {
             Node current = q.poll();
             System.out.print(current.key + " ");
-            if(current.left != null){
+            if (current.left != null) {
                 q.add(current.left);
             }
-            if(current.right != null){
+            if (current.right != null) {
                 q.add(current.right);
             }
         }
+    }
+
+    public static void printLevelLineByLine(Node root) {
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+        q.add(null);
+        while (q.size() > 1) {
+            Node curr = q.poll();
+
+            if (curr == null) {
+                System.out.println();
+                q.add(null);
+            }
+
+            else {
+                System.out.print(curr.key + " ");
+
+                if (curr.left != null)
+                    q.add(curr.left);
+
+                if (curr.right != null)
+                    q.add(curr.right);
+            }
+        }
+    }
+
+    public static void printLevelLineByLine2(Node root) {
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(root);
+
+        while (queue.isEmpty() != true) {
+            int count = queue.size();
+            for (int i = 0; i < count; i++) {
+                Node curr = queue.poll();
+                System.out.print(curr.key + " ");
+                if (curr.left != null)
+                    queue.add(curr.left);
+                if (curr.right != null)
+                    queue.add(curr.right);
+            }
+            System.out.println();
+        }
+    }
+
+    // Size non recursive algorithm
+    public static int treeSize(Node root) {
+        int count = 0;
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+        while (q.isEmpty() == false) {
+            Node curr = q.poll();
+            count++;
+
+            if (curr.left != null)
+                q.add(curr.left);
+            if (curr.right != null)
+                q.add(curr.right);
+        }
+
+        return count;
+    }
+
+    // Size of tree recursive method
+    public static int treeSizeR(Node root) {
+        if (root != null) {
+            return 1 + treeSizeR(root.left) + treeSizeR(root.right);
+        }
+        return 0;
+    }
+
+    //Maximum of Tree recursive method
+    public static int maxTree(Node root){
+        if(root == null){
+            return Integer.MIN_VALUE;
+        }
+        
+        return Math.max(root.key, Math.max(maxTree(root.left), maxTree(root.right)));
+    }
+
+    public static int maxTreeLevel(Node root){
+        if(root == null)
+            return Integer.MIN_VALUE;
+        Queue<Node> q = new LinkedList<Node>();
+        int max = root.key;
+        q.add(root);
+        while(q.isEmpty() == false){
+            Node cur = q.poll();
+            if(cur.key > max)
+                max = cur.key;
+
+            if(cur.left != null)
+                q.add(cur.left);
+            if(cur.right != null)
+                q.add(cur.right);
+        }
+
+        return max;
     }
 
     public static void main(String args[]) {
@@ -111,19 +207,22 @@ public class Program {
         root.left = new Node(20);
         root.right = new Node(30);
 
-        root.right.left = new Node(40);
-        root.right.right = new Node(50);
+        root.left.left = new Node(40);
+        root.left.right = new Node(50);
+
+        root.right.left = new Node(60);
+        root.right.right = new Node(70);
 
         // Tree
-        // 10
-        // / \
-        // 20 30
-        // /\
-        // 40 50
+        //    10
+        //   /  \
+        //  20  30
+        // / \  / \
+        // 40 50 60 70
 
-        // Inorder: 20 10 40 30 50
-        // Preorder: 10 20 30 40 50
-        // Postorder: 20 40 50 30 10
+        // Inorder: 40 20 50 10 60 30 70
+        // Preorder: 10 20 30 40 50 60 70
+        // Postorder: 40 50 20 60 70 30 10
 
         System.out.print("Inorder Tree Traversal: ");
         inorder(root);
@@ -139,7 +238,6 @@ public class Program {
 
         System.out.println("Height of the SubTree: " + heightOfTree(root));
 
-
         System.out.print("Kth level Traversal: ");
         printKthLevel(root, 2);
         System.out.println();
@@ -148,11 +246,24 @@ public class Program {
         levelOrderTraversal(root);
         System.out.println();
 
-
         System.out.println("Level order traversal Using Queue: ");
         printLevel(root);
         System.out.println();
 
+        System.out.println("Level order traversal Line by Line");
+        printLevelLineByLine(root);
+        System.out.println();
+
+        System.out.println("Level order traversal Line by Line2: ");
+        printLevelLineByLine2(root);
+
+        System.out.println("Size of the Tree is: " + treeSize(root));
+
+        System.out.println("Size of the Tree is: " + treeSizeR(root));
+
+
+        System.out.println("Maximum of the Tree is: " + maxTree(root));
+        System.out.println("Maximum of the Tree using Level Order is: " + maxTreeLevel(root));
     }
 
 }

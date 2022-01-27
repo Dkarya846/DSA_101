d31fe8352e6f1b98e20e6e078920c0e437be3962
Deepak Kumar <dkarya.delhi.uk@gmail.com>
2022-01-27 23:40:02 +0530

    added another method to count frequency of elements

--- Practice Programs/27. Frequency of Elements/Program.java
+++ Practice Programs/27. Frequency of Elements/Program.java
@@ -3,6 +3,26 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Program {
+
+    static void printFrequency(int arr[]){
+        LinkedHashMap<Integer, Integer> hashMap = new LinkedHashMap<Integer, Integer>();
+        for(int item: arr){
+            // if(hashMap.containsKey(item)){
+            //     hashMap.put(item, hashMap.get(item) + 1);
+            // }
+            // else{
+            //     hashMap.put(item, 1);
+            // }
+            //getOrDefault is used to get a key otherwise 
+            hashMap.put(item, hashMap.getOrDefault(item, 0) + 1);
+        }
+
+        // System.out.println(hashMap);
+        for(Map.Entry<Integer, Integer> entry: hashMap.entrySet()){
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
     public static void main(String [] args){
         int arr[] = {10,12,10,15,10,20,12,12};
         for (int i : arr) {
@@ -18,21 +38,5 @@ public class Program {
         System.out.println();
         printFrequency(arr2);
 
-    }
-    static void printFrequency(int arr[]){
-        LinkedHashMap<Integer, Integer> hashMap = new LinkedHashMap<Integer, Integer>();
-        for(int item: arr){
-            if(hashMap.containsKey(item)){
-                hashMap.put(item, hashMap.get(item) + 1);
-            }
-            else{
-                hashMap.put(item, 1);
-            }
-        }
-
-        // System.out.println(hashMap);
-        for(Map.Entry<Integer, Integer> entry: hashMap.entrySet()){
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
     }
 }
